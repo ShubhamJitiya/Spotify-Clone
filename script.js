1919b9fd8758ac3b979d04d50522ef315e5062f6
TechTalkerShubh <jitiya66@gmail.com>
2023-01-22 13:15:41 +0530

    Animating gif wave

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio('1.mp3')
 let masterPlay = document.getElementById('masterPlay')
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
@@ -22,10 +23,12 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
     }else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
     }
 })
 
