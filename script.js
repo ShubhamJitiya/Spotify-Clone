70b3128fd860e579ea2a1966464e530c0d39d4d4
TechTalkerShubh <jitiya66@gmail.com>
2023-01-22 13:09:48 +0530

    Play/pause btn working���

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ let songs = [
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime <= 0){
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
     }
 })
 
