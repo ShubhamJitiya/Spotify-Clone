6fc3c3a7e78a8b809dc1eab934e0473d4c5ac2e1
TechTalkerShubh <jitiya66@gmail.com>
2023-01-22 13:06:53 +0530

    Playing song on click master btn

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 console.log("Welcome to spotify")
+
+//Initialize the variables
+let songIndex = 0;
 let audioElement = new Audio('1.mp3')
+let masterPlay = document.getElementById('masterPlay')
+let myProgressBar = document.getElementById('myProgressBar');
+
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+];
+
 // audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+    }
+})
+
+//Listen to events
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate')
+    //Update seekbar
+})
+
 
